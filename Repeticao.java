fb6e517aaf839fea61b760a41dddfd969db75ab2
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2021-01-04 14:58:44 -0300

    Repetição em Java

---
+++ Repeticao.java
@@ -0,0 +1,9 @@
+public class Repeticao {
+    public static void main(String[] args) {
+        int soma = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(soma++);
+        }
+    }
+}
