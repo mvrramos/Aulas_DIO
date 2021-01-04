d7b2af26d5cfdd0b0674517303848070e55d91fa
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2021-01-04 14:11:44 -0300

    Concatenação de string

---
+++ Strings.java
@@ -0,0 +1,13 @@
+public class Strings {
+    public static void main(String[] args) {
+        
+        var nome = "Marcos";
+        var sobreNome = " Rodrigues";
+        final var nomeCompleto = nome + sobreNome;
+        
+        System.out.println(nome);
+        System.out.println("Nome do cliente: " + nome);
+        System.out.println("Nome completo do cliente: " + nomeCompleto);
+        
+    }
+}
