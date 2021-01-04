a1e8a5b365236793f3333e0023e20d7bb5020d7b
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2021-01-04 11:52:43 -0300

    confirmar em 'main'

---
+++ NãoPrimitivo.java
@@ -0,0 +1,14 @@
+/**
+ * NãoPrimitivo
+ */
+
+public class NãoPrimitivo {
+    public static void main(String[] args) {
+        String texto = "Um texto de exemplo para o código";
+        void v;
+        Object o = new Object();
+        Number numero = Integer.valueOf(100);
+        numero.toString();
+    }
+    
+}
