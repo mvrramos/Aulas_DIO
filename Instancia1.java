77d3556cbb67bf27d9b196e1876ce52c5c42ac73
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2021-01-04 12:24:11 -0300

    Exemplo de modificadores de acesso

--- Instancia1.java
+++ Instancia1.java
@@ -1,9 +1,9 @@
 public class Instancia1 {
     public static void main(String[] args) {
-        final Cachorro pitbull = new Cachorro();
+        final Instancia2 pitbull = new Instancia2();
         pitbull.zoologia = "Bipede";
 
-        final Cachorro viralatas = new Cachorro();
+        final Instancia2 viralatas = new Instancia2();
 
         System.out.println(pitbull.zoologia);
         System.out.println(viralatas.zoologia);
