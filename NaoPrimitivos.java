956ab127cfff3bd8dcd10d8dbdb8a8c48efd65ad
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2021-01-04 12:04:42 -0300

    Alteração nome do arquivo

--- NaoPrimitivos.java
+++ NaoPrimitivos.java
@@ -2,12 +2,12 @@
  * NãoPrimitivo
  */
 
-public class NãoPrimitivo {
+public class NaoPrimitivos {
     public static void main(String[] args) {
         String texto = "Um texto de exemplo para o código";
         Void v;
         Object o = new Object();
-        numero.toString();
+        Numero.toString();
         Number numero = Integer.valueOf(100);
     }
     
