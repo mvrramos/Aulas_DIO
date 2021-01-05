f2cb11ae44f068fad738846d9a32ee2e0f69f7c4
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2021-01-05 16:43:09 -0300

    Exemplos de classes e instâncias
    Signed-off-by: Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>

---
+++ Objeto.java
@@ -0,0 +1,7 @@
+public class Objeto {
+    public static void main(String[] args) {
+        //Instanciando(criando) um objeto de pessoa 
+        Pessoa pessoa = new Pessoa();
+        System.out.println("Olá, seu nome é "+ pessoa.nome);
+    }
+}
