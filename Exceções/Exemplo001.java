d07a1e0ddc2c53750dba872c5b0ca86c644e2167
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2021-01-13 16:20:19 -0300

    Exemplo de tratamento de exceções

--- Exceções/Exemplo001.java
+++ Exceções/Exemplo001.java
@@ -4,7 +4,6 @@ public class Exemplo001 {
     try {
         new java.io.FileInputStream("arquivo.txt");
     } catch (java.io.FileInputStream e) {
-        //TODO: handle exception
         System.out.println("Não foi possível abrir arquivo para leitura");
     }
 
