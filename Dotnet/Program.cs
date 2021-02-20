b87fd0d528d4cc260560cbdda1796d5f7019a36b
Marcos Rodrigues <mvrr1232@gmail.com>
2021-02-19 22:53:44 -0300

    Organização pastas

---
+++ Dotnet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DIO{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Digite a opção desejada");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair\n");
+            Console.WriteLine(" ");
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X"){
+                switch (opcaoUsuario){
+                    case "1":
+
+                        break;
+            
+                    case "2":
+                    
+                        break;
+
+                    case "3":
+                    
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
+            Console.WriteLine("Digite a opção desejada");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair\n");
+            Console.WriteLine(" ");
+        }
+    }
+}
