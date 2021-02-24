9ca5ce4cf91b3f03b4a28721589148f20f2c32c2
Marcos Rodrigues <mvrr1232@gmail.com>
2021-02-23 21:57:00 -0300

    Código em .Net + C#

---
+++ Dotnet/Desafios/Divisao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Desafios{
+    class Divisao{
+        static void Main(string[] args){
+            decimal x, y, q;
+            decimal divisao;
+
+            Console.WriteLine("Qual a quantidade de pares? ");
+            q = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < q; i++){
+                                
+                Console.WriteLine("Digite o numero 1: ");
+                x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o numero 2: ");
+                y = int.Parse(Console.ReadLine());
+
+                if (y < 0){
+                    Console.WriteLine("divisao impossivel");
+                }else{
+                    divisao = x/y;
+                    Console.WriteLine(divisao);
+                }
+            }
+
+        }
+    }
+}
